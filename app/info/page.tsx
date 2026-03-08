de96b0353a5013f9e689d34650f52d5ef9229cbb
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:47 -0500

    fix: mark header-dependent routes as dynamic

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import RsvpInfoCard from '../components/RsvpInfoCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Rsvp Infos | Wedding',
--- app/our-story/page.tsx
+++ app/our-story/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import StoryChapterCard from '../components/StoryChapterCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Story Chapters | Wedding',
